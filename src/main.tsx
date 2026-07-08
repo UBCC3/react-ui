af7963ec763ae182f795e6afde4b0e822de07807
josedamara <josedamara0211@gmail.com>
2026-07-08 14:28:10 -0700

    fix: Change redirect to the last accessed page not the main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,43 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css'
 import App from './App.jsx'
 
-const onRedirectCallback = (appState) => {
-    window.history.replaceState(
-        {},
-        document.title,
-        appState?.returnTo || window.location.pathname
-    );
-};
+const Auth0ProviderWithNavigate = ({ children }) => {
+    const navigate = useNavigate();
+
+    const onRedirectCallback = (appState) => {
+        navigate(appState?.returnTo || window.location.pathname);
+    };
+
+    return (
+        <Auth0Provider
+            domain={import.meta.env.VITE_AUTH0_DOMAIN}
+            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+            authorizationParams={{
+                redirect_uri: window.location.origin + (import.meta.env.VITE_MODE === 'development' ? '/callback' : '/ubchemica'),
+                audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+            }}
+            useRefreshTokens={true}
+            onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    )
+}
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<Auth0Provider
-				domain={import.meta.env.VITE_AUTH0_DOMAIN}
-				clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-				authorizationParams={{
-					redirect_uri: window.location.origin + (import.meta.env.VITE_MODE === 'development' ? '/callback' : '/ubchemica'),
-					audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-				}}
-                useRefreshTokens={true}
-                onRedirectCallback={onRedirectCallback}
-			>
-				<App />
-			</Auth0Provider>
+			<BrowserRouter basename={import.meta.env.VITE_MODE === 'development' ? '' : '/ubchemica'}>
+                <Auth0ProviderWithNavigate>
+                    <App />
+                </Auth0ProviderWithNavigate>
+            </BrowserRouter>
 		</StrictMode>,
 	);
 } else {
