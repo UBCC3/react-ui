27863c93d0cf94f76d03ef147f01553afcf67920
josedamara <josedamara0211@gmail.com>
2026-08-17 02:41:53 -0700

    feat: Removed cluster API url

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 // Extend Vite's ImportMetaEnv interface with your custom env variables
 interface ImportMetaEnv {
 	readonly VITE_STORAGE_API_URL: string;
-	readonly VITE_CLUSTER_API_URL: string;
 	readonly VITE_API_URL: string;
 	readonly VITE_MODE: "development" | "production";
 }
